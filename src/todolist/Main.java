c298dcc5f00693acd44891dc025aec25236333a5
Maciej Sliwinski <maciej.sliwinski@sabre.com>
2018-07-05 15:42:31 +0200

    One again to clean up

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Todo List");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
