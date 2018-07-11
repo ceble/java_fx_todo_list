76ce56a55cf36d8ee4c9c975b78c69c24913861a
Maciej Sliwinski <maciek.sliwinski@gmail.com>
2018-07-11 23:38:45 +0200

    Date format

--- src/todolist/Controller.java
+++ src/todolist/Controller.java
@@ -27,12 +27,12 @@ public class Controller {
     private Label deadlineLabel;
 
     public void initialize(){
-        TodoItem item1 = new TodoItem("First todo item", "This is details for the first item", LocalDate.of(2018,07,28));
-        TodoItem item2 = new TodoItem("2 First todo item", "2 This is details for the first item", LocalDate.of(2018,07,29));
-        TodoItem item3 = new TodoItem("3 First todo item", "3 This is details for the first item", LocalDate.of(2018,07,27));
-        TodoItem item4 = new TodoItem("4 First todo item", "4 This is details for the first item", LocalDate.of(2018,07,26));
-        TodoItem item5 = new TodoItem("5 First todo item", "5 This is details for the first item", LocalDate.of(2018,07,25));
-        TodoItem item6 = new TodoItem("6 First todo item", "6 This is details for the first item", LocalDate.of(2018,07,24));
+        TodoItem item1 = new TodoItem("First todo item", "This is details for the first item", LocalDate.of(2018,5,28));
+        TodoItem item2 = new TodoItem("2 First todo item", "2 This is details for the first item", LocalDate.of(2018,2,20));
+        TodoItem item3 = new TodoItem("3 First todo item", "3 This is details for the first item", LocalDate.of(2018,7,27));
+        TodoItem item4 = new TodoItem("4 First todo item", "4 This is details for the first item", LocalDate.of(2018,8,26));
+        TodoItem item5 = new TodoItem("5 First todo item", "5 This is details for the first item", LocalDate.of(2018,9,25));
+        TodoItem item6 = new TodoItem("6 First todo item", "6 This is details for the first item", LocalDate.of(2018,12,24));
 
         todoItems = new ArrayList<>(Arrays.asList(item1,item2,item3,item4,item5,item6));
 
@@ -42,8 +42,8 @@ public class Controller {
                 if(newValue != null){
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
-                    DateTimeFormatter df = DateTimeFormatter.ofPattern()
-                    deadlineLabel.setText(item.getDeadLine().toString());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadLine()));
 
                 }
             }
